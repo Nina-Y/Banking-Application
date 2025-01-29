a07b6e0d47bc8cc68eb920e5e31fff809707f095
Nina-Y <nina.yushchenko@gmail.com>
2025-01-29 17:37:34 +0200

    fix

--- src/main/java/com/example/banking/config/CorsConfig.java
+++ src/main/java/com/example/banking/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://banking-application-53wg.onrender.com",
+                        "http://13.60.62.171")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- src/main/java/com/example/banking/service/BankService.java
+++ src/main/java/com/example/banking/service/BankService.java
@@ -47,7 +47,7 @@ public class BankService {
             return ResponseEntity.badRequest().body("Sender account not found or insufficient funds.");
         }
 
-        String externalApiUrl = "http://13.60.62.171/api/v1/accounts/transfer/external";
+        String externalApiUrl = "https://banking-application-53wg.onrender.com/api/v1/accounts/transfer/external"; // my
 
         Map<String, Object> requestBodyMap = new HashMap<>();
         requestBodyMap.put("fromAccountNumber", senderAccountNumber);
